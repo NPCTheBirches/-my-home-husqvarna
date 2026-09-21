53ad2664a4bd9f6f64ec83ce5d0d3388616fd659
NPCTheBirches <n.casson@btinternet.com>
2026-09-21 16:37:26 +0100

    Serve index.html using path module
    Add path module and serve index.html file

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
-
+import path from "path";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("index.html"));
+});
 const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.HUSQVARNA_APPLICATION_KEY;
 const API_SECRET = process.env.HUSQVARNA_APPLICATION_SECRET;
